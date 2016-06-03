2451dc6b7d2e5d30bb417da5a1ace44ba4e1f5e3
Henry Throop <henry.throop@gmail.com>
2016-06-04 00:29:34 +0530

    Development. In general backplanes are working now, for phase, radius, longitude, etc. Some tweaking still to do -- not yet stable.

--- hbt/create_backplane.py
+++ hbt/create_backplane.py
@@ -5,220 +5,214 @@ Created on Sat May 28 20:41:09 2016
 @author: throop
 """
 
-
-import pdb
-import glob
-import math       # We use this to get pi. Documentation says math is 'always available' 
-                  # but apparently it still must be imported.
-from   subprocess import call
-import warnings
-import pdb
-import os.path
-import os
-import subprocess
-
+import math      
 import astropy
 from   astropy.io import fits
-from   astropy.table import Table
-import astropy.table   # I need the unique() function here. Why is in in table and not Table??
-import matplotlib.pyplot as plt # pyplot
-from   matplotlib.figure import Figure
 import numpy as np
-import astropy.modeling
-from   scipy.optimize import curve_fit
-#from   pylab import *  # So I can change plot size.
-                       # Pylab defines the 'plot' command
 import cspice
-import skimage
-from   itertools import izip    # To loop over groups in a table -- see astropy tables docs
-from   astropy.wcs import WCS
-from   astropy.vo.client import conesearch # Virtual Observatory, ie star catalogs
-from   astropy import units as u           # Units library
-from   astropy.coordinates import SkyCoord # To define coordinates to use in star search
-#from   photutils import datasets
-from   astropy.stats import sigma_clipped_stats
-from   scipy.stats import mode
-from   scipy.stats import linregress
-from   photutils import daofind
 import wcsaxes
 import hbt
 
-import imreg_dft as ird
-import re # Regexp
-
-# Time -- included
-# Pointing - included
-# Position -- included
-# Input: Target name
-# Possible backplanes made:
-#  o Orbital distance (at equatorial plane)
-#  o Azimuth angle (at equatorial plane)
-#  o Phase angle, solar -- will vary only a tiny bit, but put in as an error check
-#  o Sub-pt lon/lat -- no, wouldn't make any sense
-#  o x y z position of this point (as error check)
-
+from pylab import *
 # Create backplanes based on an image number. This is a stand-alone function, not part of the method
 
-# For each pixel
+# For each pixel, do the followiung:
 # o Get its RA/Dec value from WCS
 # o Compute a vector from s/c in that direction
 # o Define a plane (from planet, perp to its rotational axis)
-# o Use cspice.inrypl() to get intersection between ray and a plane. Probably returns J2K coords.
-# o Convert J2K coords to Jupiter-system coords.
+# o Use cspice.inrypl() to get intersection between ray and a plane (in Jupiter coords).
 # o Convert those coords from rectangular to planetographic.
 # o Now I have azimuth, radius, etc.
 # o Compute phase angle from the rectangular coords
-
-# This is probably the sort of things that MRS wants to avoid doing. I bet I can do 1000 coords and interpolate, and be
-# nearly as accurate as computing 1 million points.
+#   *** For LORRI, the variation in phase angle across frame is pretty small. 
 
 # Q: How do I deal with offsets?
 #    o Modify the WCS pointing to be what is correct? Though slow if I am recomputing a lot.
 #    o Shift the backplane by e.g., (-11, -18) pixels? Though will skip the edges.
 
-#def create_backplane():
-
-file = '/Users/throop/Dropbox/Data/NH_Jring/data/jupiter/level2/lor/all/lor_0034609323_0x630_sci_1.fit'
-   
-file_tm = '/Users/throop/gv/dev/gv_kernels_new_horizons.txt'  # SPICE metakernel
-
-#    arr = get_image_nh(file)
-
-w = WCS(file)
-
-header = hbt.get_image_header(file)
-
-print 'crval[i] = ' + repr(w.wcs.crval)
-
-arr = hbt.get_image_nh(file)
-
-plt.imshow(arr)
-
-# Start up SPICE
-
-cspice.furnsh(file_tm)
 # 
-# Print some values. Not sure if this will work now that I have converted it to a string, not a dictionary...
+def create_backplane(file_image, frame = 'IAU_JUPITER', name_target='Jupiter', name_observer='New Horizons'):
+    
+    "Returns a set of backplanes for a given image. The image must have WCS coords available in its header."
+    "Backplanes include navigation info for every pixel, including RA, Dec, Eq Lon, Phase, etc."
 
-hdulist = fits.open(file)
+    file = '/Users/throop/Dropbox/Data/NH_Jring/data/jupiter/level2/lor/all/lor_0034609323_0x630_sci_1.fit'
+       
+    file_tm = '/Users/throop/gv/dev/gv_kernels_new_horizons.txt'  # SPICE metakernel
+    
+    #    arr = get_image_nh(file)
+    
+    w = WCS(file)
+    
+    header = hbt.get_image_header(file_image)
+    
+#    print 'crval[i] = ' + repr(w.wcs.crval)
+    
+#    image_arr = hbt.get_image_nh(file_image)
+    
+#    plt.imshow(image_arr)
+    
+    # Start up SPICE
+    
+#    cspice.furnsh(file_tm)
 
-et = float(hdulist[0].header['SPCSCET']) # ET of mid-exposure, on s/c
-exptime = float(hdulist[0].header['EXPOSURE']) / 1000 # Convert to seconds
-utc = hdulist[0].header['SPCUTCID'] # UTC of mid-exposure, on s/c
-
-print 'ET = ' + repr(et)
-print 'EXPTIME =' + repr(exptime) + ' s'
-
-hdulist.close()
-
-# Create a plane based on Jupiter
-
-name_target = 'Jupiter'
-
-# Get the plane in Jupiter coordinates. Pretty easy!  Though we might want to do it in J2K coords
-
-plane_jup = cspice.nvp2pl([0,0,1], [0,0,0])    # nvp2pl: Normal Vec + Point to Plane
-
-# Q: How do you get the absolute position of a body... not relative to an observer, but relative to center of J2K frame?
-
-pos_jup = cspice.spkezr
-
-n_dx = 1024
-n_dy = 1024 # should get from NAXIS2
-
-lon_arr = np.zeros((n_dx, n_dy))
-lat_arr = np.zeros((n_dx, n_dy))
-radius_arr = np.zeros((n_dx, n_dy))
-ra_arr = np.zeros((n_dx, n_dy))
-dec_arr = np.zeros((n_dx, n_dy))
-
-i_x = 512
-i_y = 512
-
-stop
-# NB: at 2007-055T07:50:03.368, sub-obs lat = -6, d = 6.9 million km = 95 rj.
-# sub-obs lon/lat = 350.2 / -6.7
-
-rj = 71492  # Jupiter radius, in km. Just for q&d conversions
-
-for i_x in range(n_dx):
-    for i_y in range(n_dy):
-
-        ra, dec = w.wcs_pix2world(i_x, i_y, 0) # Compute RA, Dec for a single pixel.
+    # Print some values. Not sure if this will work now that I have converted it to a string, not a dictionary...
+    
+    hdulist = fits.open(file)
+    
+    et      = float(hdulist[0].header['SPCSCET']) # ET of mid-exposure, on s/c
+    exptime = float(hdulist[0].header['EXPOSURE']) / 1000 # Convert to seconds
+    utc     = hdulist[0].header['SPCUTCID'] # UTC of mid-exposure, on s/c
+    
+    n_dx    = int(hdulist[0].header['NAXIS1']) # Pixel dimensions of image
+    n_dy    = int(hdulist[0].header['NAXIS2'])
+    
+#    print 'ET = ' + repr(et)
+#    print 'EXPTIME =' + repr(exptime) + ' s'
+    
+    hdulist.close()
+    
+    # Look up parameters for the target body, used for PGRREC().
+    
+    radii = cspice.bodvrd(name_target, 'RADII')
+    
+    r_e = radii[0]
+    r_p = radii[2]
+    flat = (r_e - r_p) / r_e
+    
+    # Define a spice 'plane' along Jupiter's equatorial plane.
         
-        ra = float(ra)          # cspice will crash if we give it a one-element array, instead of a scalar
-        dec = float(dec)
-        
-#        print "ra,dec = " + repr(ra) + ", " + repr(dec)
-#        print "et = " + repr(et)
-
-        vec_pix_j2k =  cspice.radrec(1., ra*hbt.d2r, dec*hbt.d2r) # Vector thru pixel to ring, in J2K [args ok]
-        
-        # Q: How can I transform this vector from J2K to jupiter system coords? Can I just run PXFORM on it? probably.
-        # A vector is basically the same as a point.
-        
-        mx_j2k_jup = cspice.pxform('J2000', 'IAU_JUPITER', et) # from, to, et
-
-        # Convert vector along the pixel direction, from J2K into IAU_JUP frame
-        # I *guess* I can do this... transform a vector from one frame into another.
-  
-        vec_pix_jup = cspice.mxv(mx_j2k_jup, vec_pix_j2k)
-        
-        # Get vec from Jup to NH, in Jupiter frame
-               
-        (st_jup_sc_jup, lt) = cspice.spkezr('New Horizons', et, 'IAU_JUPITER', 'LT', 'Jupiter')
-        (st_jup_sc_j2k, lt) = cspice.spkezr('New Horizons', et, 'J2000',       'LT', 'Jupiter')
-     
-        vec_jup_sc_jup = st_jup_sc_jup[0:3]
-        vec_jup_sc_j2k = st_jup_sc_j2k[0:3]
+    # Get the plane in Jupiter coordinates. Pretty easy!  Though we might want to do it in J2K coords
+    
+    plane_jup = cspice.nvp2pl([0,0,1], [0,0,0])    # nvp2pl: Normal Vec + Point to Plane
+    
+    lon_arr = np.zeros((n_dx, n_dy))        # Longitude of pixel (defined with recpgr)
+    lat_arr = np.zeros((n_dx, n_dy))        # Latitude of pixel (which is zero, so meaningless)
+    radius_arr = np.zeros((n_dx, n_dy))     # Equatorial radius
+    ra_arr = np.zeros((n_dx, n_dy))         # RA of pixel
+    dec_arr = np.zeros((n_dx, n_dy))        # Dec of pixel
+    phase_arr = np.zeros((n_dx, n_dy))      # Phase angle    
+    
+    # Get xformation matrix from J2K to jupiter system coords. I can use this for points *or* vectors.
             
-        # Name this vector a 'point'
-        
-        pt_jup_sc_jup = vec_jup_sc_jup
-#        
-#         void spkezr_c ( ConstSpiceChar     *targ,
-#                   SpiceDouble         et,
-#                   ConstSpiceChar     *ref,
-#                   ConstSpiceChar     *abcorr,
-#                   ConstSpiceChar     *obs,
-#                   SpiceDouble         starg[6],
-#                   SpiceDouble        *lt  
-#        
-        (npts, pt_intersect_jup) = cspice.inrypl(pt_jup_sc_jup, vec_pix_jup, plane_jup) # intersect ray and plane. Jup coords.
+    mx_j2k_jup = cspice.pxform('J2000', frame, et) # from, to, et
+    
+    # Get vec from Jup to NH, in Jupiter frame (or whatever named frame is passed in)
+                   
+    (st_jup_sc_jup, lt) = cspice.spkezr(name_observer, et, frame,         'LT', name_target)
+    (st_jup_sc_j2k, lt) = cspice.spkezr(name_observer, et, 'J2000',       'LT', name_target)     
+    
+    vec_jup_sc_jup = st_jup_sc_jup[0:3]
+    vec_jup_sc_j2k = st_jup_sc_j2k[0:3]
 
-# Reality check: the vectors pt_jup_sc_jup and vec_pix_jup should both be very nearly opposite (within a degree or two)
-# They're not -- they're off by 60 deg.
-#
-#        print 'angle = ' + repr(cspice.vsep(pt_jup_sc_jup, vec_pix_jup)*hbt.r2d)
+    # Name this vector a 'point'
         
-# OK, for testing purposes, check that the angles are opposite each other when measured in J2K coords
-# OK! These are also off by 60 deg. So we can figure out this problem in J2K.
+    pt_jup_sc_jup = vec_jup_sc_jup
 
-#        print 'angle = ' + repr(cspice.vsep(vec_jup_sc_j2k, vec_pix_j2k)*hbt.r2d)
-        
-        radius, lon, lat = cspice.reclat(pt_intersect_jup)
-        
-        radius_arr[i_x, i_y] = radius
-        lon_arr[i_x, i_y] = lon
-        lat_arr[i_x, i_y] = lat
-        ra_arr[i_x, i_y] = ra
-        dec_arr[i_x, i_y] = dec
-        
-#        print 'Radius = ' + repr(radius)
+    # Get vector from Jupiter to Sun
+    # spkezr(target ... observer)
 
-#        print "ra,dec = {0},{1}; et = {2}; radius = {3}, lon/lat = {4}/{5}".format(ra, dec, et, radius, lon, lat)
-#        print
+    (st_jup_sun_jup, lt) = cspice.spkezr('Sun', et, frame, 'LT', name_target) # From Jup to Sun, in Jup frame
+    vec_jup_sun_jup = st_jup_sun_jup[0:3]
+    
+    # NB: at 2007-055T07:50:03.368, sub-obs lat = -6, d = 6.9 million km = 95 rj.
+    # sub-obs lon/lat = 350.2 / -6.7
+    
+#    rj = 71492  # Jupiter radius, in km. Just for q&d conversions
+    
+    i_y_2d = np.outer(range(n_dy), 1 + np.zeros(n_dy))
+    i_x_2d = np.transpose(i_y_2d)
+    (ra_2d, dec_2d) = w.wcs_pix2world(i_x_2d, i_y_2d, False)
+    
+    for i_x in range(n_dx):
+        for i_y in range(n_dy):
+    
+            # Look up the vector direction of this single pixel, which is defined by an RA and Dec
+    
+            vec_pix_j2k =  cspice.radrec(1., ra_2d[i_x, i_y]*hbt.d2r, dec_2d[i_x, i_y]*hbt.d2r) # Vector thru pixel to ring, in J2K [args ok]
+            
+            # Convert vector along the pixel direction, from J2K into IAU_JUP frame
+      
+            vec_pix_jup = cspice.mxv(mx_j2k_jup, vec_pix_j2k)
+    
+            # And calculate the intercept point between this vector, and the plane defined by Jupiter's equator
+                
+            (npts, pt_intersect_jup) = cspice.inrypl(pt_jup_sc_jup, vec_pix_jup, plane_jup) # intersect ray and plane. Jup coords.
+    
+            # Now calculate the phase angle: angle between s/c-to-ring, and ring-to-sun
+    
+            vec_ring_sun_jup = -pt_intersect_jup + vec_jup_sun_jup
+            
+            angle_phase = cspice.vsep(-vec_pix_jup, vec_ring_sun_jup)
+            
+            # Save various derived quantities
+            
+            lon, lat, alt = cspice.recpgr(name_target, pt_intersect_jup, r_e, flat)
+            
+            radius_arr[i_x, i_y] = alt
+            lon_arr[i_x, i_y] = lon
+            phase_arr[i_x, i_y] = angle_phase
+            
+    # Assemble the results
 
-        # For a reality check, compute the sub-observer angles. I think the GV values are computed with RECPGR,
-        # which includes flattening and using a different E-W sense.
-        
-#        (radius, lon, lat) = cspice.reclat(vec_jup_sc_jup)
-#   
-#        print 'sub-obs lon/lat = ' + repr(lon*hbt.r2d) + '/' + repr(lat*hbt.r2d)
-        
-# Get the vector from s/c, thru this pixel, thru the ring plane. Do it in Jupiter coordinates.
-        
-        # Find the intercept point between the ring plane and the vector, using cspice.inrypl()
-        # Plane and vector must             
-# How do I do that?        
-        
+    backplane = {'RA'        : ra_2d,
+                 'Dec'       : dec_2d,
+                 'Radius_eq'    : radius_arr,
+                 'Longitude_eq' : lon_arr, 
+                 'Phase'        : phase_arr}
+    
+    # And return them
+                 
+    return backplane
+
+# Now write some code to run this function. I think I can do this here?
+    
+file = '/Users/throop/Dropbox/Data/NH_Jring/data/jupiter/level2/lor/all/lor_0034609323_0x630_sci_1.fit'
+
+plane = create_backplane(file)
+
+arr_image = hbt.get_image_nh(file)
+
+hbt.set_plot_defaults()
+plt.rc('image', cmap='prism')               # Default color table for imshow
+plt.rc('image', cmap='jet')               # Default color table for imshow
+
+rcParams['figure.figsize'] = 12, 18
+
+# Make a 3 x 2 plot of all of the info, with a scalebar for each
+
+fs = 20
+
+plt.subplot(3,2,1)
+plt.imshow(arr_image)
+plt.title(file)
+
+plt.subplot(3,2,2)
+plt.imshow(plane['Phase'])
+plt.title('Phase', fontsize=fs)
+
+plt.subplot(3,2,3)
+plt.imshow(plane['RA'])
+plt.title('RA', fontsize=fs)
+
+plt.subplot(3,2,4)
+plt.imshow(plane['Dec'])
+plt.title('Dec', fontsize=fs)
+
+plt.subplot(3,2,5)
+plt.imshow(plane['Longitude_eq'])
+plt.title('Lon_eq', fontsize=fs)
+
+plt.subplot(3,2,6)
+plt.imshow(plane['Radius_eq'])
+plt.title('Radius_eq', fontsize=fs)
+
+plt.show()
+
+radius = plane['Radius_eq']
+is_ring = np.array((radius > 122000)) & np.array((radius < 129000))
+plt.imshow(is_ring + arr_image / np.max(arr.image))
+
+plt.imshow( ( np.array(radius > 61000) & np.array(radius < 64500)) + arr_image / np.max(arr_image))
+stop
+
