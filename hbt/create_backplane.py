f84733a1266ae5c3c0c8122a26a4d9874b3755ae
Henry Throop <henry.throop@gmail.com>
2016-06-05 00:58:38 +0530

    Backplane generation and ring extraction now works in this standalone code.

--- hbt/create_backplane.py
+++ hbt/create_backplane.py
@@ -148,6 +148,7 @@ def create_backplane(file_image, frame = 'IAU_JUPITER', name_target='Jupiter', n
             # Save various derived quantities
             
             lon, lat, alt = cspice.recpgr(name_target, pt_intersect_jup, r_e, flat)
+            alt, lon, lat = cspice.reclat(pt_intersect_jup)
             
             radius_arr[i_x, i_y] = alt
             lon_arr[i_x, i_y] = lon
@@ -155,8 +156,8 @@ def create_backplane(file_image, frame = 'IAU_JUPITER', name_target='Jupiter', n
             
     # Assemble the results
 
-    backplane = {'RA'        : ra_2d,
-                 'Dec'       : dec_2d,
+    backplane = {'RA'        : ra_2d * hbt.d2r, # return radians
+                 'Dec'       : dec_2d * hbt.d2r, # return radians
                  'Radius_eq'    : radius_arr,
                  'Longitude_eq' : lon_arr, 
                  'Phase'        : phase_arr}
@@ -183,36 +184,83 @@ rcParams['figure.figsize'] = 12, 18
 
 fs = 20
 
-plt.subplot(3,2,1)
-plt.imshow(arr_image)
-plt.title(file)
+fig = plt.figure()
 
-plt.subplot(3,2,2)
-plt.imshow(plane['Phase'])
-plt.title('Phase', fontsize=fs)
+ax1 = fig.add_subplot(3,2,1)
+plot1 = plt.imshow(arr_image)
+fig.colorbar(plot1)
 
-plt.subplot(3,2,3)
-plt.imshow(plane['RA'])
-plt.title('RA', fontsize=fs)
+ax2 = fig.add_subplot(3,2,2)
+plot2 = imshow(plane['Phase'] * hbt.r2d)
+plt.title('Phase [deg]', fontsize=fs)
+fig.colorbar(plot2)
 
-plt.subplot(3,2,4)
-plt.imshow(plane['Dec'])
-plt.title('Dec', fontsize=fs)
+ax3 = fig.add_subplot(3,2,3)
+plot3 = plt.imshow(plane['RA'] * hbt.r2d)
+plt.title('RA [deg]', fontsize=fs)
+fig.colorbar(plot3)
 
-plt.subplot(3,2,5)
-plt.imshow(plane['Longitude_eq'])
-plt.title('Lon_eq', fontsize=fs)
+ax4 = fig.add_subplot(3,2,4)
+plot4 = plt.imshow(plane['Dec'] * hbt.r2d)
+plt.title('Dec [deg]', fontsize=fs)
+fig.colorbar(plot4)
 
-plt.subplot(3,2,6)
-plt.imshow(plane['Radius_eq'])
-plt.title('Radius_eq', fontsize=fs)
+ax5 = fig.add_subplot(3,2,5)
+plot5 = plt.imshow(plane['Longitude_eq'] * hbt.r2d)
+plt.title('Lon_eq [deg]', fontsize=fs)
+fig.colorbar(plot5)
+
+r_j = cspice.bodvrd('JUPITER', 'RADII')
+ax6 = fig.add_subplot(3,2,6)
+plot6 = plt.imshow(plane['Radius_eq'] / r_j[0])
+plt.title('Radius_eq [$R_J$]', fontsize=fs)
+fig.colorbar(plot6)
 
 plt.show()
 
-radius = plane['Radius_eq']
-is_ring = np.array((radius > 122000)) & np.array((radius < 129000))
-plt.imshow(is_ring + arr_image / np.max(arr.image))
+# J-ring is 122,000 .. 129,000 = 1.7 .. 1.8 R_J
+radius = plane['Radius_eq'] / r_j[0]
+azimuth = plane['Longitude_eq'] * hbt.r2d
+#is_ring = np.array((radius > 122000)) & np.array((radius < 129000))
+#plt.imshow(is_ring + arr_image / np.max(arr.image))
 
-plt.imshow( ( np.array(radius > 61000) & np.array(radius < 64500)) + arr_image / np.max(arr_image))
+offset_dx = 59
+offset_dy = 79
+arr_image_roll = np.roll(np.roll(arr_image, offset_dx, axis=1), offset_dy, axis=0)
+
+plt.imshow( ( np.array(radius > 1.7) & np.array(radius < 1.8)) + arr_image_roll / np.max(arr_image))
+plt.show()
+
+rcParams['figure.figsize'] = 8,5
+
+# Generate a radial profile
+
+bin_radius = np.linspace(1.2, 2.1, num=200)
+i_ring_r = np.zeros((200))
+for i in range(np.size(bin_radius)-1):
+    is_good = ( np.array(radius > bin_radius[i]) & np.array(radius < bin_radius[i+1]) )
+    i_ring_r[i] = np.mean(arr_image_roll[is_good])
+
+plt.plot(bin_radius, i_ring_r)  
+plt.xlim((1.6,1.9))
+plt.xlabel('$R_J$', fontsize=fs)
+plt.ylabel('I', fontsize=fs)
+plt.title('J-ring Radial Profile', fontsize=fs)
+plt.show()
+  
+# Generate an azimuthal profile
+
+bin_azimuth = np.linspace(0,360, num=200)
+i_ring_az = np.zeros((200))
+for i in range(np.size(bin_azimuth)-1):
+    is_good = ( np.array(radius > 1.7) & np.array(radius < 1.9) & 
+                np.array(azimuth > bin_azimuth[i]) & np.array(azimuth < bin_azimuth[i+1]) )
+    i_ring_az[i] = np.mean(arr_image_roll[is_good])
+plt.plot(bin_azimuth, i_ring_az)
+plt.xlim((50, 150))
+plt.xlabel('Az [deg]', fontsize=fs)
+plt.ylabel('I', fontsize=fs)
+plt.title('J-ring Azimuthal Profile', fontsize=fs)
+plt.show()    
 stop
 
