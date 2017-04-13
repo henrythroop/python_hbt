462494c1dbd60704b9cc75a619aa172746b41c33
Henry Throop <henry.throop@gmail.com>
2017-04-13 22:38:13 +0530

    Development. Almost works now.

--- captions_photo2web.py
+++ captions_photo2web.py
@@ -18,40 +18,54 @@ from subprocess import check_output # Call, and return the output
 
 file_captions = 'captions.txt'
 dir_originals = 'originals'
+dir_show      = '/Users/throop/photos/Trips/Test'
+dir_originals = os.path.join(dir, 'originals', dir_show)
 
 # With no arguments, take all of the files in the 'originals' dir, and one-by-one process them.
 # Except if originals doesn't exist, then use the current directory.
 
 if (os.path.isfile(dir_originals)):
-    dir = os.path.curdir + '/' + dir_originals
+    dir = dir_originals
 else:
-    dir = os.path.curdir
+    dir = dir_show
 
-dir = '/Users/throop/photos/Trips/Test'
-
-file = os.path.join(dir, 'originals', file_captions)
-
-images = [glob.glob("*.jpg"), 
-          glob.glob("*.jpeg"),
-          glob.glob("*.JPG"),
-          glob.glob("*.JPEG")]
+images = [glob.glob(dir + "/*.jpg"), 
+          glob.glob(dir + "/*.jpeg"),
+          glob.glob(dir + "/*.JPG"),
+          glob.glob(dir + "/*.JPEG")]
 
 images = [item for sublist in images for item in sublist]
 
 # Read the existing captions file
 # It is 2 * N lines long -- with newlines separating each entry
 
-lun = open(os.path.join(dir, file_captions), 'r') # 'r' = read
-captions = lun.readlines()
-lun.close()
+#lun = open(os.path.join(dir, file_captions), 'r') # 'r' = read
+#captions = lun.readlines()
+#lun.close()
 
 # Read all the captions from JPEG files
 
 captions_new = []
 
 for image in images:
-    caption = check_output(['exiftool', '-Description', '/Users/throop/photos/Trips/Test/originals/001_IMG_5456.jpg'])
+            # Read the output, and then convert from byte into string
+    caption = check_output(['exiftool', '-Description', image]).decode("utf-8")
+    caption = caption[34:]  # Remove first few bytes from it
     
+    captions_new.append(caption)
+
+captions = captions_new
+
+# Write all captions to the output file
+
+lun = open(os.path.join(dir, file_captions), 'w')
+for caption in captions:
+    lun.write(caption)  # Damn. Gives error of 'must be str, not bytes'
+    lun.writeline("\n")
+lun.close()
+print("Wrote: " + file_captions)
+
+
 # .extend(glob.glob("*.JPG")).extend(glob.glob("*.jpeg")).extend(glob.glob("*.JPEG"))
     
 # With multiple arguments:
