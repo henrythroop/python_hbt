2a985739ec9d0d0eefa2e74d66508d20dd8a2a12
Henry Throop <henry.throop@gmail.com>
2018-02-19 10:58:57 +0530

    Update EPO list processing code to include articles / interviews.

--- process_list_epo_hbt.py
+++ process_list_epo_hbt.py
@@ -14,12 +14,17 @@ This file is to process my list of EPO talks etc into a format for Mark Sykes' W
 @author: throop
 """
 
+
+# =============================================================================
+# Process list of talks [from first tab in .numbers file]
+# =============================================================================
+
 f = open('outreach_hbt_2017', 'r')
 text = f.read()
 f.close()
 lines = text.split('\n')
 
-lines = lines[-1:0:-1]
+lines = lines[-1:0:-1] # Reverse the order
 
 for line in lines:
     if (line):
@@ -30,3 +35,23 @@ for line in lines:
         line_out = f'{date}, {city}, {country} (HBT) -- {venue}, "{title}"\n'
         print(line_out)
 print(f"\nPrinted {len(lines)} events.")    
+
+# =============================================================================
+# Process list of articles / interview [from second tab in .numbers file]
+# =============================================================================
+
+f = open('/Users/throop/git/python_hbt/articles.txt', 'r')
+text = f.read()
+f.close()
+lines = text.split('\n')
+
+lines = lines[-1:0:-1] # Reverse the order
+
+for line in lines:
+    if (line):
+        (date, city, country, type, media, author, title, url) = line.split('\t')
+        line_out = f'{date}, "{title}", {city}, {country} (HBT) -- {media}, {type}. {url}\n'
+        line_out.replace(', ,', ',')
+        print(line_out)
+        
+print(f"\nPrinted {len(lines)} articles.")
