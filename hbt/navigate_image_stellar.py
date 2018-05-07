ed352312901b8ee8dfef5fc6f330b7e8f6f100fd
Henry Throop <henry.throop@gmail.com>
2018-05-07 15:20:04 +0530

    Updated conesearch call.

--- hbt/navigate_image_stellar.py
+++ hbt/navigate_image_stellar.py
@@ -42,7 +42,9 @@ from   scipy.optimize import curve_fit
                        # Pylab defines the 'plot' command
 import spiceypy as sp
 from   astropy.wcs import WCS
-from   astropy.vo.client import conesearch # Virtual Observatory, ie star catalogs
+#from   astropy.vo.client import conesearch # Virtual Observatory, ie star catalogs
+from   astroquery.vo_conesearch import conesearch
+
 from   astropy import units as u           # Units library
 from   astropy.coordinates import SkyCoord # To define coordinates to use in star search
 from   scipy.stats import mode
